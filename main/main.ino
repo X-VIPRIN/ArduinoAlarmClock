780517900cf313ee4a06d3c08f425fa06a757435
X-VIPRIN <Hockey9192008@gmail.com>
2024-10-20 21:05:09 -0700

    Initalized project. Created Arduino main.ino

---
+++ main/main.ino
@@ -0,0 +1,15 @@
+/*
+* Colton Paul Badock; October, 2024;
+* A alarm clock system for controlling a alarm clock; No copyright, do whatever you want, just leave credit!
+*/
+
+//Execute first (main)
+void setup() {
+  
+}
+
+
+//Infite application loop.
+void loop() {
+  
+}
